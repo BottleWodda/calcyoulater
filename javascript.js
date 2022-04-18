d7eac4b0c354e1cdf0fa3b3674d4ccda6e01f2f9
Jake Lyell <jakelyell7@gmail.com>
2022-04-18 15:40:35 +1000

    first commit, basic html and css done. practicing pulling to ubuntu

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const addition = (...numbers) => numbers.reduce((a, b) => a + b);
+console.log(addition(1, 2, 3, 4, 5));
+console.log(addition(1));
+console.log(addition(1, 2, 3));
+
+function subtraction() {
+    
+}
+
+function multiplication() {
+    
+}
+
+function division() {
+    
+}
+
+function clear() {
+
+}
