a5128badb20c8331a2ca9f7ef25bc54fe7369b1c
Jake Lyell <jakelyell7@gmail.com>
2022-04-28 19:43:02 +1000

    Messed up commits, but this should have it back.

--- javascript.js
+++ javascript.js
@@ -9,37 +9,57 @@ const upperDisplay = document.querySelector('.upperDisplay');
 let displayValue;
 let operation = null;
 let firstOperand = [];
-let firstNumber = null;
+let firstNumber;
+let secondOperand = [];
+let secondNumber;
 let result;
-let totalOperation = [];
 
-// Operation Functions
-const add = (...num) => num.reduce((a, b) => (a + b));
-const subtract = (...num) => num.reduce((a, b) => (a - b));
-const multiply = (...num) => num.reduce((a, b) => (a * b));
-const divide = (...num) => num.reduce((a, b) => (a / b));
+//Operation functions
+function add(num1, num2) {
+    return +num1 + +num2;
+}
 //
-function operate(...num) {
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+//
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+//
+function divide(num1, num2) {
+    return num1 / num2;
+}
+//
+function operate(num1, operator, num2) {
     operator = operation;
+    num1 = firstNumber;
+    num2 = secondNumber;
     if (operator == '+') {
-        return result = add(...num);
+        return result = add(num1, num2);
     } else if (operator == '-') {
-        return result = subtract(...num);
+        return result = subtract(num1, num2);
     } else if (operator == '*') {
-        return result = multiply(...num);
+        return result = multiply(num1, num2);
     } else {
-        return result = divide(...num);
+        return result = divide(num1, num2);
     }
 }
 
+
+
 //Button events
-//
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        firstOperand.push(button.value);
-        firstNumber = firstOperand.join('');
-        display.innerHTML = firstNumber;
-        operation = null;
+        if (operation !== null) {
+            secondOperand.push(button.value);
+            secondNumber = secondOperand.join('');
+            display.innerHTML = secondNumber;
+        } else {
+            firstOperand.push(button.value);
+            firstNumber = firstOperand.join('');
+            display.innerHTML = firstNumber;
+        };
     })
 });
 //
@@ -47,17 +67,14 @@ operatorButtons.forEach((button) => {
     button.addEventListener('click', () => {
         display.innerHTML = button.value;
         operation = button.value;
-        totalOperation.push(firstNumber, operation)
-        firstOperand.length = 0;
     })
 });
 //
 equalsButton.addEventListener('click', () => {
-    if (operation == null) {
-        totalOperation.push(firstNumber);
-    }
-    result = operate(totalOperation.join(''));
+    operate();
     display.innerHTML = result;
+    secondOperand.length = 0;
+    firstNumber = result;
     operation = null;
 });
 //
@@ -66,5 +83,5 @@ clear.addEventListener('click', () => {
     upperDisplay.innerHTML = '0';
     operation = null;
     firstOperand.length = 0;
-    totalOperation.length = 0;
+    secondOperand.length = 0;
 });
